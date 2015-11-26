1248e1f763b0e11f8c904810da446f6c5ec48d9f
MarkDev01 <markgermain@btinternet.com>
2015-11-26 10:18:14 +0000

    added decl_chapter01.h

---
+++ Cpp14/Chapter01/decl_chapter01.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#if !defined(CHAPTER01_DLL)
+	#if defined(CHAPTER01_BUILD)
+		#define CHAPTER01_DLL __declspec(dllexport)
+	#else
+		#define CHAPTER01_DLL __declspec(dllimport)
+	#endif
+#endif
