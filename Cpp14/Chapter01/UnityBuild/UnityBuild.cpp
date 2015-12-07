38f8de5c6c7f33438ac39b43c9577951c9c3796f
MarkDev01 <markgermain@btinternet.com>
2015-12-07 11:39:36 +0000

    Unity build working on chapter01 Standard builds now broken

---
+++ Cpp14/Chapter01/UnityBuild/UnityBuild.cpp
@@ -0,0 +1,9 @@
+// UnityBuild.cpp : source file that includes all project cpp files
+// except stdafx.cpp
+
+// stdafx.h essential for precompiled headers
+#include "stdafx.h"
+
+#include "../dllmain.cpp"
+#include "../Chapter01.cpp"
+
