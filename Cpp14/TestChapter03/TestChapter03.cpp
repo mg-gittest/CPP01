e6db9f20fa713fd2da8d7c445a32543d13c55de7
Mark Germain <mg-gittest>
2016-01-13 15:23:28 +0000

    Added TestChapter03

---
+++ Cpp14/TestChapter03/TestChapter03.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestChapter03
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(Chapter3Test01)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
